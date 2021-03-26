7753277e6ed59134fe3f55bdc83fb9b9df2f45af
Ricardo vieira da Silva <ricvsweb@gmail.com>
2021-03-26 08:23:02 -0300

    Commit inicial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import Layout from "../components/Layout"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <h1>Hello</h1>
+      <Component {...pageProps} />
+    </Layout>
+    )
 }
 
 export default MyApp
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const about = ()=>{
+
+	return (
+		<div>
+			<h1>This is the about page</h1>
+		</div>
+	)
+}
+
+export default about
